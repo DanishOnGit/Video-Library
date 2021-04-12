3f13c3b60648e1891de97dc0300eaef3a4d89bd5
Danish Ahmed <65118743+DanishOnGit@users.noreply.github.com>
2021-04-12 14:39:59 +0530

    feat:interactions added

--- src/Components/ReactPlayer.js
+++ src/Components/ReactPlayer.js
@@ -1,9 +1,35 @@
 import { useParams } from "react-router-dom";
-import ReactPlayer from 'react-player/youtube';
+import ReactPlayer from "react-player/youtube";
 
+export const VideoPlayer = () => {
+  const { videoId } = useParams();
+  return (
+    <>
+    <div className="video-player-wrapper">
+      <ReactPlayer
+        controls={true}
+        url={`https://www.youtube.com/watch?${videoId}`}
+      />
+      <div className="interactions-wrapper">
 
-export const VideoPlayer=()=>{
-  
-const {videoId}=useParams();
-  return <ReactPlayer controls={true}  url={`https://www.youtube.com/watch?${videoId}`} />
-}
+        <ul className="list-items-flex list-non-bullet spaced">
+          <li>
+            <i class="fas fa-thumbs-up"></i>
+          </li>
+          <li>
+            <i class="fas fa-list"></i>
+          </li>
+          <li>
+            <i class="fas fa-clock"></i>
+          </li>
+        </ul>
+
+        <ul className="interactions-wrapper__note list-non-bullet">
+          <li><i class="fas fa-pen"></i> Add Notes</li>
+        </ul>
+
+      </div>
+      </div>
+    </>
+  );
+};
